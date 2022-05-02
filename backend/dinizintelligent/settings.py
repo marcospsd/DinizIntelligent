639d69321ca8d3c5fcf64983c472f98c892535a5
marcospsd <marcosp_sd@hotmail.com>
2022-05-02 14:22:15 -0300

    vai

--- backend/dinizintelligent/settings.py
+++ backend/dinizintelligent/settings.py
@@ -81,9 +81,17 @@ WSGI_APPLICATION = 'dinizintelligent.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dinizintelligence',
+        'USER': 'root',
+        'PASSWORD': 'T1$3nh4',
+        'PORT': '3306',
+        'HOST': '10.3.1.5',
     }
 }
 
