28b061896e2fe0148d16d8154e2879af3804ac64
marcospsd <marcosp_sd@hotmail.com>
2022-05-12 03:39:25 -0300

    vai

--- frontintelligent/src/pages/Home/Relatorios/ADM/Exagerado2022.jsx
+++ frontintelligent/src/pages/Home/Relatorios/ADM/Exagerado2022.jsx
@@ -8,7 +8,7 @@ const Exagerado2022 = () => {
     <>
         <ResponsiveAppBar/>
         <div className='container'>
-        <iframe title="exagerado maio 2022 - Dash board" id="relatorios" src="https://app.powerbi.com/view?r=eyJrIjoiZDhhYTc5NzQtMzFkYi00NGQxLTkwY2YtNGUyYmNhZTY3OTY5IiwidCI6IjY1OGMyZTAyLTQ5NTQtNGNhNy1iNWU5LTRlNTBlYzk3OWFlYSJ9" frameborder="0" allowFullScreen="true"></iframe>
+        <iframe title="exagerado maio 2022 - Dash board" id="relatorios" src="https://app.powerbi.com/view?r=eyJrIjoiNGI0NzA4ZGMtMmIyYi00MDk5LTg0OWQtMjU2MjQzYjliOTNhIiwidCI6IjY1OGMyZTAyLTQ5NTQtNGNhNy1iNWU5LTRlNTBlYzk3OWFlYSJ9" frameborder="0" allowFullScreen="true"></iframe>
         </div>
     </>
 
