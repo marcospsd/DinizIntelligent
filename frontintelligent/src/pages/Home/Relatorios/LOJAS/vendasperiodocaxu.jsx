12a4f2f8c530c3ab3a9c5c999628b96195110932
marcospsd <marcosp_sd@hotmail.com>
2022-07-04 15:51:00 -0300

    ok

--- frontintelligent/src/pages/Home/Relatorios/LOJAS/vendasperiodocaxu.jsx
+++ frontintelligent/src/pages/Home/Relatorios/LOJAS/vendasperiodocaxu.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
+import { useContext } from 'react';
 import ResponsiveAppBar from '../../../../components/navbar/navbar'
+import { AuthContext } from '../../../../services/auth';
 
 
 const VendasPeriodoCaxu = () => {
+    const { user } = useContext(AuthContext)
+
+    const loja = () => {
+        switch (user.setor) {
+            case "2001":
+                return "https://app.powerbi.com/view?r=eyJrIjoiNTZjYTMwMjEtZmQ2ZC00YjU3LWEyMTEtOGFjOGRmMDk2NDFhIiwidCI6IjY1OGMyZTAyLTQ5NTQtNGNhNy1iNWU5LTRlNTBlYzk3OWFlYSJ9&pageName=ReportSectionb94c3f676cb056712910"
+            default:
+                return;
+            }
+    }
 
     return (
     <>
         <ResponsiveAppBar/>
         <div className='container'>
-        <iframe title="xavier" id='relatorios' src="https://app.powerbi.com/view?r=eyJrIjoiNTZjYTMwMjEtZmQ2ZC00YjU3LWEyMTEtOGFjOGRmMDk2NDFhIiwidCI6IjY1OGMyZTAyLTQ5NTQtNGNhNy1iNWU5LTRlNTBlYzk3OWFlYSJ9&pageName=ReportSectionb94c3f676cb056712910" frameborder="0" allowFullScreen="true"></iframe>
+        <iframe title="xavier" id='relatorios' src={loja()} frameborder="0" allowFullScreen="true"></iframe>
         </div>
     
     </>
