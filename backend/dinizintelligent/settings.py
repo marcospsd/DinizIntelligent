07b68d4ad0bb5a26e1fd0f0e47f2d204c74da182
Marcos Paulo <marcosp_sd@hotmail.com>
2024-08-23 15:50:03 -0300

    vai

--- backend/dinizintelligent/settings.py
+++ backend/dinizintelligent/settings.py
@@ -159,8 +159,8 @@ AUTH_USER_MODEL = 'users.User'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        #'rest_framework.authentication.BasicAuthentication',
+        #'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
